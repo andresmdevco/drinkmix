51768f3088542eb9d6056b03837c4bce5b91b500
Andrés Muñoz <andres2078@gmail.com>
2026-08-19 14:23:49 -0500

    feat: validate recipe API response with Zod

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,15 +1,17 @@
 import type { StateCreator } from 'zustand';
 import { getCategories, getRecipes } from '../services/RecipeService';
-import type { Categories, SearchFilter } from '../types';
+import type { Categories, Drinks, SearchFilter } from '../types';
 
 export type RecipesSliceType = {
   categories: Categories;
+  drinks: Drinks;
   fetchCategories: () => Promise<void>;
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>;
 };
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
   categories: { drinks: [] },
+  drinks: { drinks: [] },
   fetchCategories: async () => {
     const categories = await getCategories();
     set({
@@ -17,6 +19,9 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     });
   },
   searchRecipes: async (filters) => {
-    await getRecipes(filters);
+    const drinks = await getRecipes(filters);
+    set({
+      drinks,
+    });
   },
 });

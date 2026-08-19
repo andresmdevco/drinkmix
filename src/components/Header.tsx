668f1f5efda99c9969d6477d93994218efd4862e
Andrés Muñoz <andres2078@gmail.com>
2026-08-19 10:29:38 -0500

    feat: fetch drink categories

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import { useAppStore } from '../stores/useAppStore';
 
 export default function Header() {
   const { pathname } = useLocation();
   const isHome = useMemo(() => pathname === '/', [pathname]);
+
+  const fetchCategories = useAppStore((state) => state.fetchCategories)
+
+  useEffect(() => {
+     fetchCategories()
+  }, [])
 
   return (
     <header className={isHome ? 'bg-[url(/bg.jpg)] bg-center bg-cover' : 'bg-slate-800'}>

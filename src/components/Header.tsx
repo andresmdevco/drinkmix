7780e5cf4946d0a909c77e00cb82a9488236020b
Andrés Muñoz <andres2078@gmail.com>
2026-08-19 13:47:02 -0500

    handle search form submission

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, type ChangeEvent } from 'react';
+import { useEffect, useMemo, useState, type ChangeEvent, type SubmitEvent } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useAppStore } from '../stores/useAppStore';
 
@@ -12,6 +12,7 @@ export default function Header() {
 
   const fetchCategories = useAppStore((state) => state.fetchCategories);
   const categories = useAppStore((state) => state.categories);
+  const searchRecipes = useAppStore((state) => state.searchRecipes);
 
   useEffect(() => {
     fetchCategories();
@@ -22,6 +23,19 @@ export default function Header() {
       ...searchFilters,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: Validar
+    if (Object.values(searchFilters).includes('')) {
+      console.log('Todos los campos son obligatorios');
+      return;
+    }
+
+    // Consultar las recetas
+    searchRecipes(searchFilters);
   };
 
   return (
@@ -53,7 +67,10 @@ export default function Header() {
         </div>
 
         {isHome && (
-          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+          <form
+            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+            onSubmit={handleSubmit}
+          >
             <div className="space-y-4">
               <label
                 htmlFor="ingredient"
--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,10 +1,11 @@
 import type { StateCreator } from 'zustand';
 import { getCategories } from '../services/RecipeService';
-import type { Categories } from '../types';
+import type { Categories, SearchFilter } from '../types';
 
 export type RecipesSliceType = {
   categories: Categories;
   fetchCategories: () => Promise<void>;
+  searchRecipes: (searchFilters: SearchFilter) => Promise<void>;
 };
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
@@ -14,5 +15,8 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     set({
       categories,
     });
+  },
+  searchRecipes: async (filters) => {
+    console.log(filters);
   },
 });

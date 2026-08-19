a3085851009ea3cb2fc79ad2d583d7c0047ca978
Andrés Muñoz <andres2078@gmail.com>
2026-08-18 19:05:08 -0500

    feat: create application header

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom' 
-import IndexPage from './views/IndexPage'
-import FavoritesPage from './views/FavoritesPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import IndexPage from './views/IndexPage';
+import FavoritesPage from './views/FavoritesPage';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<IndexPage />} />
-        <Route path='/favoritos' element={<FavoritesPage />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/favoritos" element={<FavoritesPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }

ad31497e57d517a92f2f457bfd8631482d48831a
Andrés Muñoz <andres2078@gmail.com>
2026-08-19 13:56:08 -0500

    feat: search recipes by category and ingredient

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import type { StateCreator } from 'zustand';
-import { getCategories } from '../services/RecipeService';
+import { getCategories, getRecipes } from '../services/RecipeService';
 import type { Categories, SearchFilter } from '../types';
 
 export type RecipesSliceType = {
@@ -17,6 +17,6 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     });
   },
   searchRecipes: async (filters) => {
-    console.log(filters);
+    await getRecipes(filters);
   },
 });

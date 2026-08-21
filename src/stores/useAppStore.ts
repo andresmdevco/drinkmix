32727b4576dc75334cc9014f0d33dd06e9b0aca4
Andrés Muñoz <andres2078@gmail.com>
2026-08-20 22:24:29 -0500

    feat: add notification slice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,10 +2,12 @@ import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, type RecipesSliceType } from './recipeSlice';
 import { createFavoritesSlice, type FavoritesSliceType } from './favoritesSlice';
+import { createNotificationSlice, type NotificationSliceType } from './notificationSlice';
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
   })),
 );

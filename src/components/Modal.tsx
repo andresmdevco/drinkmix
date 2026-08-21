9d5a3c955fab3ba3b2b56fca90a4b6d473a9f6bc
Andrés Muñoz <andres2078@gmail.com>
2026-08-20 19:06:37 -0500

    feat: display recipe ingredients and measurements

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,30 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, type JSX } from 'react';
 import { useAppStore } from '../stores/useAppStore';
+import type { Recipe } from '../types';
 
 export default function Modal() {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+
+  const renderIngredients = () => {
+    const ingredients: JSX.Element[] = []
+    for (let i = 1; i <= 15; i++) {
+      const ingredient = selectedRecipe[`strIngredient${i}` as keyof Recipe]
+      const measure = selectedRecipe[`strMeasure${i}` as keyof Recipe]
+
+      if (ingredient && measure) {
+        ingredients.push(
+          <li key={i} className='text-lg font-normal list-inside'>
+            {ingredient} - {measure}
+          </li>
+        )
+      }
+    }
+
+    return ingredients
+  }
 
   return (
     <>
@@ -49,6 +68,7 @@ export default function Modal() {
                   <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Ingredientes y Cantidades
                   </DialogTitle>
+                  {renderIngredients()}
                   <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Instrucciones
                   </DialogTitle>

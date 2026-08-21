abf8c4e3fca2948e1312c821128adb2864eb08e0
Andrés Muñoz <andres2078@gmail.com>
2026-08-20 20:41:34 -0500

    feat: conditionally display favorite action

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,6 +8,7 @@ export default function Modal() {
   const closeModal = useAppStore((state) => state.closeModal);
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
   const handleClickFavorite = useAppStore((state) => state.handleClickFavorite);
+  const favoriteExists = useAppStore((state) => state.favoriteExists);
 
   const renderIngredients = () => {
     const ingredients: JSX.Element[] = [];
@@ -92,7 +93,9 @@ export default function Modal() {
                       className="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
                       onClick={() => handleClickFavorite(selectedRecipe)}
                     >
-                      Agregar a favoritos
+                      {favoriteExists(selectedRecipe.idDrink)
+                        ? 'Eliminar de Favoritos'
+                        : 'Agregar a Favoritos'}
                     </button>
                   </div>
                 </DialogPanel>

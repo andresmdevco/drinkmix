923602c9f0292af87438d934a33f94392d652d2f
Andrés Muñoz <andres2078@gmail.com>
2026-08-22 14:01:12 -0500

    style: update header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
-            <img className="w-32" src="/logo.svg" alt="logotipo" />
+            <img className="w-52" src="/logo.png" alt="logotipo" />
           </div>
 
           <nav className="flex gap-4">

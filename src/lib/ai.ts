4f3d2ba6f9039acc606a313c7ad927386680ec0a
Andrés Muñoz <andres2078@gmail.com>
2026-08-24 22:00:15 -0500

    feat: add AI service for recipte generation

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,5 +1,5 @@
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 
-export const openroute = createOpenRouter({
+export const openrouter = createOpenRouter({
   apiKey: import.meta.env.VITE_OPENROUTER_KEY,
 });
--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -1,14 +1,14 @@
-import type { StateCreator } from 'zustand'
+import type { StateCreator } from 'zustand';
+import AIService from '../services/AIService';
 
 export type AISlice = {
-  recipe: string
-  generateRecipe: (prompt: string) => Promise<void>
-}
+  recipe: string;
+  generateRecipe: (prompt: string) => Promise<void>;
+};
 
 export const createAISlice: StateCreator<AISlice, [], [], AISlice> = () => ({
   recipe: '',
-  generateRecipe: async(prompt) => {
-
-    console.log(prompt)
-  }
-})
+  generateRecipe: async (prompt) => {
+    await AIService.generateRecipe(prompt);
+  },
+});

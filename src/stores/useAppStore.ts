57a6fd385f8a2ed18759f5df34b16a489774ab2d
Andrés Muñoz <andres2078@gmail.com>
2026-08-24 20:19:54 -0500

    feat: add AI slice to app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,11 +3,13 @@ import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, type RecipesSliceType } from './recipeSlice';
 import { createFavoritesSlice, type FavoritesSliceType } from './favoritesSlice';
 import { createNotificationSlice, type NotificationSliceType } from './notificationSlice';
+import { createAISlice, type AISlice } from './aiSlice';
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
     ...createNotificationSlice(...a),
+    ...createAISlice(...a),  
   })),
 );
